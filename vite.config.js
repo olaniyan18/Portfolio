38009a12206d6f44263d36d003b8bbafb877663b
Your Name <your.email@example.com>
2026-05-31 11:15:27 +0100

    fixed vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,4 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 5173,
-    open: true,
-  },
-  build: {
-    outDir: "dist",
-    sourcemap: false,
-    minify: "esbuild",
-  },
 });
